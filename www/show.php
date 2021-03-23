9f216fd00321b4f6e035be2ba0df1618124e34dc
topspot101 <54053583+topspot101@users.noreply.github.com>
2021-03-23 00:53:53 +0000

    Create show.php
    Add show.php

---
+++ www/show.php
@@ -0,0 +1,147 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html lang="en-US" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title>XxXGreenMonXxX</title>
+<meta http-equiv="content-type" content="text/html; charset=iso-8859-1" >
+<meta name="dc.language" scheme="rfc1766" content="en-US" >
+<meta name="robots" content="noindex,follow" >
+<META HTTP-EQUIV="Refresh" CONTENT="300" >
+<META HTTP-EQUIV="Pragma" CONTENT="no-cache" >
+<link rel="stylesheet" type="text/css" href="/w3_common.css">
+
+<script language="JavaScript1.2" type="text/javascript">
+</script>
+</head>
+<body EXPIRES="+5s" bgcolor="#000000" marginheight="0" marginwidth="0" leftmargin="0" topmargin="0" >
+      <table width="100%" cellpadding="0" cellspacing="0" border="0" >
+        <tr>
+	        <td bgcolor="#999999">&nbsp;</td>
+		<td bgcolor="#999999"><img src="/spacer.gif" alt="" width="39" height="15" border="0" /></td>
+		<td bgcolor="#999999">
+		<div align="right"><span class="site-title">Sparky!</span></div>
+		</td>
+		<td bgcolor="#999999">&nbsp;</td>
+	</tr>
+        <tr>
+        <td valign="top" ><img src="/spacer.gif" alt="" width="140" height="15" border="0" />
+        &nbsp;</td>
+        <td width="100%" valign="top"><p class="head-bluedark"><!-- #BeginEditable "pageTitle"-->
+        <table width="100%"><tr><td width="50%" align="left">
+        <p class="head-bluedark">GreenMon Graphs</p>
+        </td><td width="50%" align="right">
+        </td></tr>
+        <tr> <td colspan=2 align="center"><font color="#000CC"><b>XxX</b><font color="#CC0033"><b>GreenMon Data</b><font color="#000CC"><b>XxX</b>  </td></tr>
+        </table>
+        <!-- #EndEditable --></p> </td>
+        <td></td> 
+        <td bgcolor="#999999">&nbsp;</td>
+        </tr>
+
+        <tr> 
+          <td valign="top">
+
+          </td>
+          <td valign="top" align="center">
+              <table border="0" cellspacing="0" cellpadding="3">
+                <tr> <td valign="top">
+		 <table border="0" width="100%" cellspacing="0" cellpadding="1" bordercolor="#000000">
+		  <tr bgcolor="#000000">
+          <td><img src="/spacer.gif" alt="" width="4" height="2" border="0" /></td>
+          </tr>
+          <tr bgcolor="#aaaaaa">
+          <td><img src="/spacer.gif" alt="" width="4" height="15" border="0" /></td>
+          <td align="center"><b><font size=+1 color="#000000">Data from GreenMon</font></b></td>
+          <td><img src="/spacer.gif" alt="" width="4" height="15" border="0" /></td>
+          </tr>
+          <tr bgcolor="#aaaaaa">
+          <td><img src="/spacer.gif" alt="" width="4" height="15" border="0" /></td>
+          <td><table border="0" width="100%" cellspacing="0" cellpadding="1" bordercolor="#000000">
+		  <tr bgcolor="#006699">
+		  <td><img src="/spacer.gif" alt="" width="4" height="15" border="0" /></td>
+		  <td><font color="#ffffff"><b>One Day graph</b>  (Five Minute Average)</font></td>
+		  <td><img src="/spacer.gif" alt="" width="4" height="15" border="0" /></td>
+		  </tr>
+		  <tr bgcolor="#006699">
+		  <td><img src="/spacer.gif" alt="" width="4" height="15" border="0" /></td>
+                  <td><img src="image.php?time=day&type=temphum" alt="temphum Not Found"/><br/>
+		  <img src="image.php?time=day&type=irSurfaceTemp" alt="irSurfaceTemp Not Found"/></br>
+		  <img src="image.php?time=day&type=vpd" alt="vpd Not Found"/></br>
+                  <img src="image.php?time=day&type=light" alt="light Not Found"/></br>
+		  <img src="image.php?time=day&type=ikInputOutput" alt="ikInputOutput Not Found"/></br>
+		  <td><img src="/spacer.gif" alt="" width="4" height="15" border="0" /></td>
+		  </tr>
+		  <tr bgcolor="#006699">
+		  <td><img src="/spacer.gif" alt="" width="4" height="15" border="0" /></td>
+		  <td><font color="#ffffff"><b>One Week graph</b>  (Thirty Minute Average)</font></td>
+		  <td><img src="/spacer.gif" alt="" width="4" height="15" border="0" /></td>
+		  </tr>
+		  <tr bgcolor="#006699">
+		  <td><img src="/spacer.gif" alt="" width="4" height="15" border="0" /></td>
+                  <td><img src="image.php?time=week&type=temphum" alt="temphum Not Found"/></br>
+		  <img src="image.php?time=week&type=irSurfaceTemp" alt="irSurfaceTemp Not Found"/></br>
+		  <img src="image.php?time=week&type=vpd" alt="vpd Not Found"/></br>
+                  <img src="image.php?time=week&type=light" alt="light Not Found"/></br>
+                  <img src="image.php?time=week&type=ikInputOutput" alt="ikInputOutput Not Found"/></br>
+		  <td><img src="/spacer.gif" alt="" width="4" height="15" border="0" /></td>
+		  </tr>
+		  <tr bgcolor="#006699">
+		  <td><img src="/spacer.gif" alt="" width="4" height="15" border="0" /></td>
+		  <td><font color="#ffffff"><b>One Month graph</b>  (Two Hour Average)</font></td>
+		  <td><img src="/spacer.gif" alt="" width="4" height="15" border="0" /></td>
+		  </tr>
+		  <tr bgcolor="#006699">
+		  <td><img src="/spacer.gif" alt="" width="4" height="15" border="0" /></td>
+                  <td><img src="image.php?time=month&type=temphum" alt="temphum Not Found"/></br>
+		  <img src="image.php?time=month&type=irSurfaceTemp" alt="irSurfaceTemp Not Found"/></br>
+		  <img src="image.php?time=month&type=vpd" alt="vpd Not Found"/></br>
+                  <img src="image.php?time=month&type=light" alt="light Not Found"/></br>
+                  <img src="image.php?time=month&type=ikInputOutput" alt="ikInputOutput Not Found"/></br>
+		  <td><img src="/spacer.gif" alt="" width="4" height="15" border="0" /></td>
+		  </tr>
+		  <tr bgcolor="#006699">
+		  <td><img src="/spacer.gif" alt="" width="4" height="15" border="0" /></td>
+		  <td><font color="#ffffff"><b>One Year graph</b>  (One Day Average)</font></td>
+		  <td><img src="/spacer.gif" alt="" width="4" height="15" border="0" /></td>
+		  </tr>
+		  <tr bgcolor="#006699">
+		  <td><img src="/spacer.gif" alt="" width="4" height="15" border="0" /></td>
+                  <td><img src="image.php?time=year&type=temphum" alt="temphum Not Found"/></br>
+		  <img src="image.php?time=year&type=irSurfaceTemp" alt="irSurfaceTemp Not Found"/></br>
+		  <img src="image.php?time=year&type=vpd" alt="vpd Not Found"/></br>
+                  <img src="image.php?time=year&type=light" alt="light Not Found"/></br>
+                  <img src="image.php?time=year&type=ikInputOutput" alt="ikInputOutput Not Found"/></br>
+		  <td><img src="/spacer.gif" alt="" width="4" height="15" border="0" /></td>
+		  </tr>
+                  <tr bgcolor="#006699">
+             <td colspan="5"><img src="/spacer.gif" alt="" width="4" height="3" border="0" /></td>
+          </tr>
+          </table></td>
+           <td><img src="/spacer.gif" alt="" width="4" height="15" border="0" /></td>
+          </tr>
+          <tr bgcolor="#aaaaaa">
+             <td colspan="3"><img src="/spacer.gif" alt="" width="4" height="3" border="0" /></td>
+          </tr>
+		 
+		 </table></td>
+		 </tr>
+              </table><br>
+            <p>&nbsp;</p>
+            </td>
+	    <td></td>
+	<td bgcolor="#999999">
+        <table width="100%" cellpadding="0" cellspacing="0" border="0">
+        <tr>
+                  <td><img src="/spacer.gif" alt="" width="62" height="200" border="0" /></td>
+        </tr>
+        <tr>
+                  <td align="center"><img src="/greenmonLogo.gif" alt="GrMn" width="44" height="15" border="0" /></td>
+        </tr>
+        <tr>
+                  <td><img src="/spacer.gif" alt="" width="62" height="500" border="0" /></td>
+        </tr>
+        </table></td>
+
+        </tr>
+      </table>
+</body>
+</html>
